6f289529ebfe6dd8567d022ecca27b3a284d5f6e
Matthew Juel <matthewjuel@Matthews-MacBook-Pro.local>
2020-01-23 22:03:56 -0800

    Initial Commit

---
+++ Crash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Crash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
